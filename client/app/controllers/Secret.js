1cbface6daabe0d34ed6617f5edd4fea88f69c4f
David Konsumer <konsumer@jetboystudio.com>
2015-08-23 19:14:34 -0700

    forgot to "lock down" Secret

--- client/app/controllers/Secret.js
+++ client/app/controllers/Secret.js
@@ -1,3 +1,7 @@
-angular.module('demoApp').controller('SecretController', function($scope){
-
+angular.module('demoApp').controller('SecretController', function($scope, $location, authonice){
+  authonice.user().then(function(user){
+    $scope.email = user.email;
+  }, function(){
+    $location.path('/');
+  });
 });
