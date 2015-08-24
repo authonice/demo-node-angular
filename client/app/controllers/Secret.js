aa4b49861afa4700a417e566d92bff4ca3f4c25f
David Konsumer <konsumer@jetboystudio.com>
2015-08-23 19:15:08 -0700

    comment

--- client/app/controllers/Secret.js
+++ client/app/controllers/Secret.js
@@ -1,4 +1,8 @@
 angular.module('demoApp').controller('SecretController', function($scope, $location, authonice){
+  /**
+   * In a production-app, all actual secret content should come form a locked-down server-side endpoint.
+   * This isn't secure, it's just for illustrative purposes.
+   */
   authonice.user().then(function(user){
     $scope.email = user.email;
   }, function(){
